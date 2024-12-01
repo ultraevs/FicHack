25a43bd8d415e5ec4e927ed5e330ea9dfc7175a6
Ilya Lomaev <ilyalomaev146@gmail.com>
2024-12-01 15:21:16 +0300

    Added possibility of upload lots of files

--- frontend/fic_app/src/components/TableRow/TableRow.tsx
+++ frontend/fic_app/src/components/TableRow/TableRow.tsx
@@ -1,10 +1,45 @@
 import { FC } from "react";
 import classes from "./TableRow.module.css";
-interface TableRowProps {
-    isHead?: boolean;
+import StateLabel from "../StateLabel/StateLabel";
+import { State } from "../StateLabel/StateLabel";
+
+export interface RawsData {
+    avgConf: number;
+    objects: string;
+    timeTaken: number | string;
 }
 
-const TableRow: FC<TableRowProps> = ({ isHead }) => {
+export interface TableRowProps {
+    isHead?: boolean;
+    state: State;
+    nameOfFile?: string;
+    data?: RawsData;
+}
+
+const TableRow: FC<TableRowProps> = ({ isHead, state, data, nameOfFile }) => {
+    function getFormattedDate(): string {
+        const date = new Date();
+        const months = [
+            "янв",
+            "фев",
+            "мар",
+            "апр",
+            "мая",
+            "июн",
+            "июл",
+            "авг",
+            "сент",
+            "окт",
+            "нояб",
+            "дек",
+        ];
+
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const hours = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        return `${day} ${month}. ${hours}:${minutes}`;
+    }
     if (isHead) {
         return (
             <div className={classes.headRow}>
@@ -15,7 +50,15 @@ const TableRow: FC<TableRowProps> = ({ isHead }) => {
             </div>
         );
     }
-    return <div></div>;
+    return (
+        <div className={classes.row}>
+            <span>ID 3210</span>
+            <StateLabel state={state} />
+            <span>{nameOfFile}</span>
+            <span>{data?.objects}</span>
+            <span>{getFormattedDate()}</span>
+        </div>
+    );
 };
 
 export default TableRow;
